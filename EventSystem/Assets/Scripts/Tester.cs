c623cb1c6ee33e26e5cadcae5623e8e669304b3f
Julen <julen.zabala@hotmail.com>
2021-01-13 19:57:18 +0100

    - Created EventManager with suscribe, unsuscribe and raise methods for 2 types of actions

---
+++ EventSystem/Assets/Scripts/Tester.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.instance.SuscribeToEvent("MouseClick", MouseClick);
+        EventManager.instance.SuscribeToEvent("MouseClick", MouseClicka);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            EventManager.instance.RaiseEvent("MouseClick");
+            EventManager.instance.RaiseEvent("MouseClick", 5);
+        }
+        if (Input.GetMouseButtonDown(1))
+            EventManager.instance.UnsuscribeFromEvent("MouseClick", MouseClick);
+    }
+
+    private void MouseClick()
+    {
+        Debug.Log("Mouse click!");
+    }
+    
+    private void MouseClicka(int i)
+    {
+        Debug.Log("Mouse click!" + i);
+    }
+}
